491da6a77832af1260c35b9a5c79e58921d317f2
aviv-piotrwojtowicz <piotr.wojtowicz-ext@aviv-group.com>
2026-06-08 23:59:42 +0200

    nowe dest

--- src/Entities.cpp
+++ src/Entities.cpp
@@ -11,49 +11,49 @@ using namespace std;
 void initPassengers() {
     int predefinedDestinations[PASSENGERS_AMOUNT] = {
         DESTINATIONS[0],  // Card 1
-        DESTINATIONS[1],  // Card 2
-        DESTINATIONS[2],  // Card 3
-        DESTINATIONS[3],  // Card 4
-        DESTINATIONS[4],  // Card 5
-        DESTINATIONS[5],  // Card 6
-        DESTINATIONS[6],  // Card 7
-        DESTINATIONS[7],  // Card 8
-        DESTINATIONS[8],  // Card 9
-        DESTINATIONS[9],  // Card 10
+        DESTINATIONS[12],  // Card 2
+        DESTINATIONS[11],  // Card 3
+        DESTINATIONS[15],  // Card 4
+        DESTINATIONS[11],  // Card 5
+        DESTINATIONS[8],  // Card 6
+        DESTINATIONS[12],  // Card 7
+        DESTINATIONS[14],  // Card 8
+        DESTINATIONS[3],  // Card 9
+        DESTINATIONS[5],  // Card 10
         DESTINATIONS[10], // Card 11
-        DESTINATIONS[11], // Card 12
-        DESTINATIONS[12], // Card 13
-        DESTINATIONS[13], // Card 14
-        DESTINATIONS[14], // Card 15
-        DESTINATIONS[15], // Card 16
-        DESTINATIONS[16], // Card 17
-        DESTINATIONS[17], // Card 18
-        DESTINATIONS[0],  // Card 19 (duplicate)
-        DESTINATIONS[1],  // Card 20 (duplicate)
-        DESTINATIONS[2],  // Card 21 (duplicate)
-        DESTINATIONS[3],  // Card 22 (duplicate)
-        DESTINATIONS[4],  // Card 23 (duplicate) 
-        DESTINATIONS[5],  // Card 24 (duplicate)
-        DESTINATIONS[6],  // Card 25 (duplicate)
-        DESTINATIONS[7],  // Card 26 (duplicate)
-        DESTINATIONS[8],  // Card 27 (duplicate)
-        DESTINATIONS[9],  // Card 28 (duplicate)
-        DESTINATIONS[10], // Card 29 (duplicate)
-        DESTINATIONS[11], // Card 30 (duplicate)
-        DESTINATIONS[12], // Card 31 (duplicate)
-        DESTINATIONS[13], // Card 32 (duplicate)
-        DESTINATIONS[14], // Card 33 (duplicate)
-        DESTINATIONS[15], // Card 34 (duplicate)
-        DESTINATIONS[16], // Card 35 (duplicate)
-        DESTINATIONS[17], // Card 36 (duplicate)
-        DESTINATIONS[0],  // Card 37 (duplicate)
-        DESTINATIONS[1],  // Card 38 (duplicate)
-        DESTINATIONS[2],  // Card 39 (duplicate)
-        DESTINATIONS[3],  // Card 40 (duplicate)
-        DESTINATIONS[4],  // Card 41 (duplicate)
-        DESTINATIONS[5],  // Card 42 (duplicate)
-        DESTINATIONS[6],  // Card 43 (duplicate)
-        DESTINATIONS[7]   // Card 44 (duplicate)
+        DESTINATIONS[2], // Card 12
+        DESTINATIONS[7], // Card 13
+        DESTINATIONS[14], // Card 14
+        DESTINATIONS[17], // Card 15
+        DESTINATIONS[10], // Card 16
+        DESTINATIONS[1], // Card 17
+        DESTINATIONS[11], // Card 18
+        DESTINATIONS[15],  // Card 19 (duplicate)
+        DESTINATIONS[8],  // Card 20 (duplicate)
+        DESTINATIONS[6],  // Card 21 (duplicate)
+        DESTINATIONS[7],  // Card 22 (duplicate)
+        DESTINATIONS[9],  // Card 23 (duplicate) 
+        DESTINATIONS[10],  // Card 24 (duplicate)
+        DESTINATIONS[2],  // Card 25 (duplicate)
+        DESTINATIONS[4],  // Card 26 (duplicate)
+        DESTINATIONS[5],  // Card 27 (duplicate)
+        DESTINATIONS[16],  // Card 28 (duplicate)
+        DESTINATIONS[7], // Card 29 (duplicate)
+        DESTINATIONS[16], // Card 30 (duplicate)
+        DESTINATIONS[2], // Card 31 (duplicate)
+        DESTINATIONS[5], // Card 32 (duplicate)
+        DESTINATIONS[0], // Card 33 (duplicate)
+        DESTINATIONS[1], // Card 34 (duplicate)
+        DESTINATIONS[15], // Card 35 (duplicate)
+        DESTINATIONS[9], // Card 36 (duplicate)
+        DESTINATIONS[12],  // Card 37 (duplicate)
+        DESTINATIONS[8],  // Card 38 (duplicate)
+        DESTINATIONS[6],  // Card 39 (duplicate)
+        DESTINATIONS[4],  // Card 40 (duplicate)
+        DESTINATIONS[3],  // Card 41 (duplicate)
+        DESTINATIONS[13],  // Card 42 (duplicate)
+        DESTINATIONS[13],  // Card 43 (duplicate)
+        DESTINATIONS[17]   // Card 44 (duplicate)
     };
 
     for(int i = 0; i < PASSENGERS_AMOUNT; i++) {
